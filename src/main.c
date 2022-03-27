5658a9b77b306246558ae86b0e780dca0318eedc
MunoLike <sukorice@gmail.com>
2022-03-28 08:30:46 +0900

    evaluate APSTA Mode and add setup-dashbutton.py

--- src/main.c
+++ src/main.c
@@ -1,96 +1,119 @@
-/*  WiFi softAP Example
+/* The example of ESP-IDF
+ *
+ * This sample code is in the public domain.
+ */
 
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include <string.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-//#include "esp_mac.h"
-#include "esp_wifi.h"
+
 #include "esp_event.h"
 #include "esp_log.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/event_groups.h"
+#include "freertos/task.h"
 #include "nvs_flash.h"
 
-#include "lwip/err.h"
-#include "lwip/sys.h"
+#define CONFIG_AP_WIFI_SSID "Text5000"
+#define CONFIG_AP_WIFI_PASSWORD "NjhB-kjfY-W5"
+#define CONFIG_AP_MAX_STA_CONN 3
+#define CONFIG_AP_WIFI_CHANNEL 11
+#define CONFIG_STA_CONNECT_TIMEOUT 10
 
-/* The examples use WiFi configuration that you can set via project configuration menu.
+static const char *TAG = "MAIN";
 
-   If you'd rather not, just change the below entries to strings with
-   the config you want - ie #define EXAMPLE_WIFI_SSID "mywifissid"
-*/
-#define EXAMPLE_ESP_WIFI_SSID      "TestAP"
-#define EXAMPLE_ESP_WIFI_PASS      "TestAPDao"
-#define EXAMPLE_ESP_WIFI_CHANNEL   11
-#define EXAMPLE_MAX_STA_CONN       3
+static EventGroupHandle_t wifi_event_group;
+const int CONNECTED_BIT = BIT0;
 
-static const char *TAG = "wifi softAP";
-
-static void wifi_event_handler(void* arg, esp_event_base_t event_base,
-                                    int32_t event_id, void* event_data)
-{
-    if (event_id == WIFI_EVENT_AP_STACONNECTED) {
-        wifi_event_ap_staconnected_t* event = (wifi_event_ap_staconnected_t*) event_data;
-        ESP_LOGI(TAG, "station "MACSTR" join, AID=%d",
-                 MAC2STR(event->mac), event->aid);
-    } else if (event_id == WIFI_EVENT_AP_STADISCONNECTED) {
-        wifi_event_ap_stadisconnected_t* event = (wifi_event_ap_stadisconnected_t*) event_data;
-        ESP_LOGI(TAG, "station "MACSTR" leave, AID=%d",
-                 MAC2STR(event->mac), event->aid);
-    }
+static void event_handler(void *arg, esp_event_base_t event_base,
+                          int32_t event_id, void *event_data) {
+  if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+    ESP_LOGI(TAG, "WIFI_EVENT_STA_DISCONNECTED");
+    esp_wifi_connect();
+    xEventGroupClearBits(wifi_event_group, CONNECTED_BIT);
+  } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+    ESP_LOGI(TAG, "IP_EVENT_STA_GOT_IP");
+    xEventGroupSetBits(wifi_event_group, CONNECTED_BIT);
+  }
 }
 
-void wifi_init_softap(void)
-{
-    ESP_ERROR_CHECK(esp_netif_init());
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
-    esp_netif_create_default_wifi_ap();
+static void initialise_wifi(void) {
+  esp_log_level_set("wifi", ESP_LOG_WARN);
+  static bool initialized = false;
+  if (initialized) {
+    return;
+  }
+  ESP_ERROR_CHECK(esp_netif_init());
+  wifi_event_group = xEventGroupCreate();
+  ESP_ERROR_CHECK(esp_event_loop_create_default());
+  esp_netif_t *ap_netif = esp_netif_create_default_wifi_ap();
+  assert(ap_netif);
+  esp_netif_t *sta_netif = esp_netif_create_default_wifi_sta();
+  assert(sta_netif);
+  wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+  ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+  ESP_ERROR_CHECK(esp_event_handler_register(
+      WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, &event_handler, NULL));
+  ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP,
+                                             &event_handler, NULL));
 
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+  ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+  ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_NULL));
+  ESP_ERROR_CHECK(esp_wifi_start());
 
-    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
-                                                        ESP_EVENT_ANY_ID,
-                                                        &wifi_event_handler,
-                                                        NULL,
-                                                        NULL));
-
-    wifi_config_t wifi_config = {
-        .ap = {
-            .ssid = EXAMPLE_ESP_WIFI_SSID,
-            .ssid_len = strlen(EXAMPLE_ESP_WIFI_SSID),
-            .channel = EXAMPLE_ESP_WIFI_CHANNEL,
-            .password = EXAMPLE_ESP_WIFI_PASS,
-            .max_connection = EXAMPLE_MAX_STA_CONN,
-            .authmode = WIFI_AUTH_WPA_WPA2_PSK
-        },
-    };
-    if (strlen(EXAMPLE_ESP_WIFI_PASS) == 0) {
-        wifi_config.ap.authmode = WIFI_AUTH_OPEN;
-    }
-
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
-    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_AP, &wifi_config));
-    ESP_ERROR_CHECK(esp_wifi_start());
-
-    ESP_LOGI(TAG, "wifi_init_softap finished. SSID:%s password:%s channel:%d",
-             EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS, EXAMPLE_ESP_WIFI_CHANNEL);
+  initialized = true;
 }
 
-void app_main(void)
-{
-    //Initialize NVS
-    esp_err_t ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-      ESP_ERROR_CHECK(nvs_flash_erase());
-      ret = nvs_flash_init();
-    }
-    ESP_ERROR_CHECK(ret);
+static bool wifi_apsta(int timeout_ms) {
+  wifi_config_t ap_config = {0};
+  strcpy((char *)ap_config.ap.ssid, CONFIG_AP_WIFI_SSID);
+  strcpy((char *)ap_config.ap.password, CONFIG_AP_WIFI_PASSWORD);
+  ap_config.ap.authmode = WIFI_AUTH_WPA_WPA2_PSK;
+  ap_config.ap.ssid_len = strlen(CONFIG_AP_WIFI_SSID);
+  ap_config.ap.max_connection = CONFIG_AP_MAX_STA_CONN;
+  ap_config.ap.channel = CONFIG_AP_WIFI_CHANNEL;
 
-    ESP_LOGI(TAG, "ESP_WIFI_MODE_AP");
-    wifi_init_softap();
+  if (strlen(CONFIG_AP_WIFI_PASSWORD) == 0) {
+    ap_config.ap.authmode = WIFI_AUTH_OPEN;
+  }
+
+  // wifi_config_t sta_config = { 0 };
+  // strcpy((char *)sta_config.sta.ssid, CONFIG_STA_WIFI_SSID);
+  // strcpy((char *)sta_config.sta.password, CONFIG_STA_WIFI_PASSWORD);
+
+  ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_APSTA));
+  ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_AP, &ap_config));
+  // ESP_ERROR_CHECK( esp_wifi_set_config(ESP_IF_WIFI_STA, &sta_config) );
+  ESP_ERROR_CHECK(esp_wifi_start());
+  ESP_LOGI(TAG, "WIFI_MODE_AP started. SSID:%s password:%s channel:%d",
+           CONFIG_AP_WIFI_SSID, CONFIG_AP_WIFI_PASSWORD,
+           CONFIG_AP_WIFI_CHANNEL);
+
+  ESP_ERROR_CHECK(esp_wifi_connect());
+  int bits = xEventGroupWaitBits(wifi_event_group, CONNECTED_BIT, pdFALSE,
+                                 pdTRUE, timeout_ms / portTICK_PERIOD_MS);
+  ESP_LOGI(TAG, "bits=%x", bits);
+  if (bits) {
+    ESP_LOGI(TAG, "WIFI_MODE_STA connected. SSID:%s password:%s", "CENSORED",
+             "CENSORED");
+  } else {
+    ESP_LOGI(TAG, "WIFI_MODE_STA can't connected. SSID:%s password:%s",
+             "CENSORED", "CENSORED");
+  }
+  return (bits & CONNECTED_BIT) != 0;
+}
+
+void app_main() {
+  esp_err_t err = nvs_flash_init();
+  if (err == ESP_ERR_NVS_NO_FREE_PAGES ||
+      err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+    ESP_ERROR_CHECK(nvs_flash_erase());
+    err = nvs_flash_init();
+  }
+  ESP_ERROR_CHECK(err);
+
+  initialise_wifi();
+
+  ESP_LOGW(TAG, "Start APSTA Mode");
+  wifi_apsta(CONFIG_STA_CONNECT_TIMEOUT * 1000);
 }
