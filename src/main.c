04e0f7a5a1e4d7fa3b04d07a1e3be064882df2ca
MunoLike <sukorice@gmail.com>
2022-03-28 19:48:27 +0900

    connect the dash(not working)

--- src/main.c
+++ src/main.c
@@ -14,8 +14,6 @@
 #include "freertos/task.h"
 #include "nvs_flash.h"
 
-#define CONFIG_AP_WIFI_SSID "Text5000"
-#define CONFIG_AP_WIFI_PASSWORD "NjhB-kjfY-W5"
 #define CONFIG_AP_MAX_STA_CONN 3
 #define CONFIG_AP_WIFI_CHANNEL 11
 #define CONFIG_STA_CONNECT_TIMEOUT 10
@@ -24,6 +22,8 @@ static const char *TAG = "MAIN";
 
 static EventGroupHandle_t wifi_event_group;
 const int CONNECTED_BIT = BIT0;
+
+struct netconn *conn;
 
 static void event_handler(void *arg, esp_event_base_t event_base,
                           int32_t event_id, void *event_data) {
@@ -34,6 +34,8 @@ static void event_handler(void *arg, esp_event_base_t event_base,
   } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
     ESP_LOGI(TAG, "IP_EVENT_STA_GOT_IP");
     xEventGroupSetBits(wifi_event_group, CONNECTED_BIT);
+  } else if(event_base == WIFI_EVENT && event_id == WIFI_EVENT_AP_STACONNECTED){
+    ESP_LOGI(TAG, "WIFI_EVENT_AP_STACONNECTED");
   }
 }
 
@@ -57,6 +59,9 @@ static void initialise_wifi(void) {
   ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP,
                                              &event_handler, NULL));
 
+  ESP_ERROR_CHECK(esp_event_handler_register(
+      WIFI_EVENT, WIFI_EVENT_AP_STACONNECTED, &event_handler, NULL));
+
   ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
   ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_NULL));
   ESP_ERROR_CHECK(esp_wifi_start());
@@ -64,30 +69,30 @@ static void initialise_wifi(void) {
   initialized = true;
 }
 
-static bool wifi_apsta(int timeout_ms) {
-  wifi_config_t ap_config = {0};
-  strcpy((char *)ap_config.ap.ssid, CONFIG_AP_WIFI_SSID);
-  strcpy((char *)ap_config.ap.password, CONFIG_AP_WIFI_PASSWORD);
-  ap_config.ap.authmode = WIFI_AUTH_WPA_WPA2_PSK;
-  ap_config.ap.ssid_len = strlen(CONFIG_AP_WIFI_SSID);
-  ap_config.ap.max_connection = CONFIG_AP_MAX_STA_CONN;
-  ap_config.ap.channel = CONFIG_AP_WIFI_CHANNEL;
 
-  if (strlen(CONFIG_AP_WIFI_PASSWORD) == 0) {
-    ap_config.ap.authmode = WIFI_AUTH_OPEN;
-  }
+static bool wifi_apsta(int timeout_ms) {
+  // wifi_config_t ap_config = {0};
+  // strcpy((char *)ap_config.ap.ssid, CONFIG_AP_WIFI_SSID);
+  // strcpy((char *)ap_config.ap.password, CONFIG_AP_WIFI_PASSWORD);
+  // ap_config.ap.authmode = WIFI_AUTH_WPA_WPA2_PSK;
+  // ap_config.ap.ssid_len = strlen(CONFIG_AP_WIFI_SSID);
+  // ap_config.ap.max_connection = CONFIG_AP_MAX_STA_CONN;
+  // ap_config.ap.channel = CONFIG_AP_WIFI_CHANNEL;
+
+  // if (strlen(CONFIG_AP_WIFI_PASSWORD) == 0) {
+  //   ap_config.ap.authmode = WIFI_AUTH_OPEN;
+  // }
 
   // wifi_config_t sta_config = { 0 };
   // strcpy((char *)sta_config.sta.ssid, CONFIG_STA_WIFI_SSID);
   // strcpy((char *)sta_config.sta.password, CONFIG_STA_WIFI_PASSWORD);
 
   ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_APSTA));
-  ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_AP, &ap_config));
-  // ESP_ERROR_CHECK( esp_wifi_set_config(ESP_IF_WIFI_STA, &sta_config) );
+  // ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_AP, &ap_config));
+  //  ESP_ERROR_CHECK( esp_wifi_set_config(ESP_IF_WIFI_STA, &sta_config) );
   ESP_ERROR_CHECK(esp_wifi_start());
   ESP_LOGI(TAG, "WIFI_MODE_AP started. SSID:%s password:%s channel:%d",
-           CONFIG_AP_WIFI_SSID, CONFIG_AP_WIFI_PASSWORD,
-           CONFIG_AP_WIFI_CHANNEL);
+           "CENSORED", "CENSORED", CONFIG_AP_WIFI_CHANNEL);
 
   ESP_ERROR_CHECK(esp_wifi_connect());
   int bits = xEventGroupWaitBits(wifi_event_group, CONNECTED_BIT, pdFALSE,
