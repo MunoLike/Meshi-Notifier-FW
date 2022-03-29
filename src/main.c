e3842666c1a31c4b9732b4cf29e697d0e7a40901
MunoLike <sukorice@gmail.com>
2022-03-29 15:32:01 +0900

    test sniffer mode

--- src/main.c
+++ src/main.c
@@ -1,124 +1,147 @@
-/* The example of ESP-IDF
- *
- * This sample code is in the public domain.
+/**
+ * Copyright (c) 2017, Łukasz Marcin Podkalicki <lpodkalicki@gmail.com>
+ * ESP32/016
+ * WiFi Sniffer.
  */
 
-#include <string.h>
-
+#include "driver/gpio.h"
 #include "esp_event.h"
-#include "esp_log.h"
+#include "esp_event_loop.h"
 #include "esp_system.h"
 #include "esp_wifi.h"
+#include "esp_wifi_types.h"
 #include "freertos/FreeRTOS.h"
-#include "freertos/event_groups.h"
-#include "freertos/task.h"
 #include "nvs_flash.h"
 
-#define CONFIG_AP_MAX_STA_CONN 3
-#define CONFIG_AP_WIFI_CHANNEL 11
-#define CONFIG_STA_CONNECT_TIMEOUT 10
+#define LED_GPIO_PIN GPIO_NUM_5
+#define WIFI_CHANNEL_MAX (13)
+#define WIFI_CHANNEL_SWITCH_INTERVAL (500)
 
-static const char *TAG = "MAIN";
+static wifi_country_t wifi_country = {
+    .cc = "JP", .schan = 1, .nchan = 14, .policy = WIFI_COUNTRY_POLICY_AUTO};
 
-static EventGroupHandle_t wifi_event_group;
-const int CONNECTED_BIT = BIT0;
+typedef struct {
+  unsigned frame_ctrl : 16;
+  unsigned duration_id : 16;
+  uint8_t addr1[6]; /* receiver address */
+  uint8_t addr2[6]; /* sender address */
+  uint8_t addr3[6]; /* filtering address */
+  unsigned sequence_ctrl : 16;
+  uint8_t addr4[6]; /* optional */
+} wifi_ieee80211_mac_hdr_t;
 
-struct netconn *conn;
+typedef struct {
+  wifi_ieee80211_mac_hdr_t hdr;
+  uint8_t payload[0]; /* network data ended with 4 bytes csum (CRC32) */
+} wifi_ieee80211_packet_t;
 
-static void event_handler(void *arg, esp_event_base_t event_base,
-                          int32_t event_id, void *event_data) {
-  if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
-    ESP_LOGI(TAG, "WIFI_EVENT_STA_DISCONNECTED");
-    esp_wifi_connect();
-    xEventGroupClearBits(wifi_event_group, CONNECTED_BIT);
-  } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
-    ESP_LOGI(TAG, "IP_EVENT_STA_GOT_IP");
-    xEventGroupSetBits(wifi_event_group, CONNECTED_BIT);
-  } else if(event_base == WIFI_EVENT && event_id == WIFI_EVENT_AP_STACONNECTED){
-    ESP_LOGI(TAG, "WIFI_EVENT_AP_STACONNECTED");
+static esp_err_t event_handler(void *ctx, system_event_t *event);
+static void wifi_sniffer_init(void);
+static void wifi_sniffer_set_channel(uint8_t channel);
+static const char *wifi_sniffer_packet_type2str(
+    wifi_promiscuous_pkt_type_t type);
+static void wifi_sniffer_packet_handler(void *buff,
+                                        wifi_promiscuous_pkt_type_t type);
+
+void app_main(void) {
+  uint8_t level = 0, channel = 1;
+
+  /* setup */
+  wifi_sniffer_init();
+  gpio_set_direction(LED_GPIO_PIN, GPIO_MODE_OUTPUT);
+
+  /* loop */
+  while (true) {
+    gpio_set_level(LED_GPIO_PIN, level ^= 1);
+    vTaskDelay(WIFI_CHANNEL_SWITCH_INTERVAL / portTICK_PERIOD_MS);
+    wifi_sniffer_set_channel(channel);
+    channel = (channel % WIFI_CHANNEL_MAX) + 1;
   }
 }
 
-static void initialise_wifi(void) {
-  esp_log_level_set("wifi", ESP_LOG_WARN);
-  static bool initialized = false;
-  if (initialized) {
-    return;
-  }
-  ESP_ERROR_CHECK(esp_netif_init());
-  wifi_event_group = xEventGroupCreate();
-  ESP_ERROR_CHECK(esp_event_loop_create_default());
-  esp_netif_t *ap_netif = esp_netif_create_default_wifi_ap();
-  assert(ap_netif);
-  esp_netif_t *sta_netif = esp_netif_create_default_wifi_sta();
-  assert(sta_netif);
+esp_err_t event_handler(void *ctx, system_event_t *event) { return ESP_OK; }
+
+void wifi_sniffer_init(void) {
+  nvs_flash_init();
+  tcpip_adapter_init();
+  ESP_ERROR_CHECK(esp_event_loop_init(event_handler, NULL));
   wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
   ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-  ESP_ERROR_CHECK(esp_event_handler_register(
-      WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, &event_handler, NULL));
-  ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP,
-                                             &event_handler, NULL));
-
-  ESP_ERROR_CHECK(esp_event_handler_register(
-      WIFI_EVENT, WIFI_EVENT_AP_STACONNECTED, &event_handler, NULL));
-
+  ESP_ERROR_CHECK(esp_wifi_set_country(
+      &wifi_country)); /* set country for channel range [1, 13] */
   ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
   ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_NULL));
   ESP_ERROR_CHECK(esp_wifi_start());
-
-  initialized = true;
+  esp_wifi_set_promiscuous(true);
+  esp_wifi_set_promiscuous_rx_cb(&wifi_sniffer_packet_handler);
 }
 
-
-static bool wifi_apsta(int timeout_ms) {
-  // wifi_config_t ap_config = {0};
-  // strcpy((char *)ap_config.ap.ssid, CONFIG_AP_WIFI_SSID);
-  // strcpy((char *)ap_config.ap.password, CONFIG_AP_WIFI_PASSWORD);
-  // ap_config.ap.authmode = WIFI_AUTH_WPA_WPA2_PSK;
-  // ap_config.ap.ssid_len = strlen(CONFIG_AP_WIFI_SSID);
-  // ap_config.ap.max_connection = CONFIG_AP_MAX_STA_CONN;
-  // ap_config.ap.channel = CONFIG_AP_WIFI_CHANNEL;
-
-  // if (strlen(CONFIG_AP_WIFI_PASSWORD) == 0) {
-  //   ap_config.ap.authmode = WIFI_AUTH_OPEN;
-  // }
-
-  // wifi_config_t sta_config = { 0 };
-  // strcpy((char *)sta_config.sta.ssid, CONFIG_STA_WIFI_SSID);
-  // strcpy((char *)sta_config.sta.password, CONFIG_STA_WIFI_PASSWORD);
-
-  ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_APSTA));
-  // ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_AP, &ap_config));
-  //  ESP_ERROR_CHECK( esp_wifi_set_config(ESP_IF_WIFI_STA, &sta_config) );
-  ESP_ERROR_CHECK(esp_wifi_start());
-  ESP_LOGI(TAG, "WIFI_MODE_AP started. SSID:%s password:%s channel:%d",
-           "CENSORED", "CENSORED", CONFIG_AP_WIFI_CHANNEL);
-
-  ESP_ERROR_CHECK(esp_wifi_connect());
-  int bits = xEventGroupWaitBits(wifi_event_group, CONNECTED_BIT, pdFALSE,
-                                 pdTRUE, timeout_ms / portTICK_PERIOD_MS);
-  ESP_LOGI(TAG, "bits=%x", bits);
-  if (bits) {
-    ESP_LOGI(TAG, "WIFI_MODE_STA connected. SSID:%s password:%s", "CENSORED",
-             "CENSORED");
-  } else {
-    ESP_LOGI(TAG, "WIFI_MODE_STA can't connected. SSID:%s password:%s",
-             "CENSORED", "CENSORED");
-  }
-  return (bits & CONNECTED_BIT) != 0;
+void wifi_sniffer_set_channel(uint8_t channel) {
+  esp_wifi_set_channel(channel, WIFI_SECOND_CHAN_NONE);
 }
 
-void app_main() {
-  esp_err_t err = nvs_flash_init();
-  if (err == ESP_ERR_NVS_NO_FREE_PAGES ||
-      err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-    ESP_ERROR_CHECK(nvs_flash_erase());
-    err = nvs_flash_init();
+const char *wifi_sniffer_packet_type2str(wifi_promiscuous_pkt_type_t type) {
+  switch (type) {
+    case WIFI_PKT_MGMT:
+      return "MGMT";
+    case WIFI_PKT_DATA:
+      return "DATA";
+    default:
+    case WIFI_PKT_MISC:
+      return "MISC";
   }
-  ESP_ERROR_CHECK(err);
+}
 
-  initialise_wifi();
+void wifi_sniffer_packet_handler(void *buff, wifi_promiscuous_pkt_type_t type) {
+  if (type != WIFI_PKT_MGMT) return;
 
-  ESP_LOGW(TAG, "Start APSTA Mode");
-  wifi_apsta(CONFIG_STA_CONNECT_TIMEOUT * 1000);
+  const wifi_promiscuous_pkt_t *ppkt = (wifi_promiscuous_pkt_t *)buff;
+  const wifi_ieee80211_packet_t *ipkt =
+      (wifi_ieee80211_packet_t *)ppkt->payload;
+  const wifi_ieee80211_mac_hdr_t *hdr = &ipkt->hdr;
+
+//   if (ipkt->hdr.addr2[0] == 0x34 && ipkt->hdr.addr2[1] == 0xd2 &&
+//       ipkt->hdr.addr2[2] == 0x70 && ipkt->hdr.addr2[3] == 0x3a &&
+//       ipkt->hdr.addr2[4] == 0xfa && ipkt->hdr.addr2[5] == 0xb5) {
+//     printf(
+//         "PACKET TYPE=%s, CHAN=%02d, RSSI=%02d,"
+//         " ADDR1=%02x:%02x:%02x:%02x:%02x:%02x,"
+//         " ADDR2=%02x:%02x:%02x:%02x:%02x:%02x,"
+//         " ADDR3=%02x:%02x:%02x:%02x:%02x:%02x\n",
+//         wifi_sniffer_packet_type2str(type), ppkt->rx_ctrl.channel,
+//         ppkt->rx_ctrl.rssi,
+//         /* ADDR1 */
+//         hdr->addr1[0], hdr->addr1[1], hdr->addr1[2], hdr->addr1[3],
+//         hdr->addr1[4], hdr->addr1[5],
+//         /* ADDR2 */
+//         hdr->addr2[0], hdr->addr2[1], hdr->addr2[2], hdr->addr2[3],
+//         hdr->addr2[4], hdr->addr2[5],
+//         /* ADDR3 */
+//         hdr->addr3[0], hdr->addr3[1], hdr->addr3[2], hdr->addr3[3],
+//         hdr->addr3[4], hdr->addr3[5]);
+//   }
+
+  //   if (ipkt->hdr.addr2[0] == 0xb4 && ipkt->hdr.addr2[1] == 0x7c &&
+  //       ipkt->hdr.addr2[2] == 0x9c && ipkt->hdr.addr2[3] == 0xae &&
+  //       ipkt->hdr.addr2[4] == 0xad && ipkt->hdr.addr2[5] == 0xe7) {
+  //     printf("frame_ctrl=%04x\n", hdr->frame_ctrl);
+  //     if (((hdr->frame_ctrl & 0x00f0) >> 4) == 0x04) {
+  //       printf(
+  //           "PACKET TYPE=%s, CHAN=%02d, RSSI=%02d,"
+  //           " ADDR1=%02x:%02x:%02x:%02x:%02x:%02x,"
+  //           " ADDR2=%02x:%02x:%02x:%02x:%02x:%02x,"
+  //           " ADDR3=%02x:%02x:%02x:%02x:%02x:%02x\n",
+  //           wifi_sniffer_packet_type2str(type), ppkt->rx_ctrl.channel,
+  //           ppkt->rx_ctrl.rssi,
+  //           /* ADDR1 */
+  //           hdr->addr1[0], hdr->addr1[1], hdr->addr1[2], hdr->addr1[3],
+  //           hdr->addr1[4], hdr->addr1[5],
+  //           /* ADDR2 */
+  //           hdr->addr2[0], hdr->addr2[1], hdr->addr2[2], hdr->addr2[3],
+  //           hdr->addr2[4], hdr->addr2[5],
+  //           /* ADDR3 */
+  //           hdr->addr3[0], hdr->addr3[1], hdr->addr3[2], hdr->addr3[3],
+  //           hdr->addr3[4], hdr->addr3[5]);
+  //     }
+  //   }
 }
